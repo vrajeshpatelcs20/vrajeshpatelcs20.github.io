5dccd8ce0c5e3d7bcce24a5bb0f26982760a1275
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-09-24 12:09:50 -0600

    added traffic Light

---
+++ trafficLight/sketch.js
@@ -0,0 +1,18 @@
+// First Demo
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255,0,255);
+  fill("red")
+  ellipse(mouseX,mouseY,100,50);
+
+}

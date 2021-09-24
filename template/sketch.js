36e613998425068a64a9e7e95b839a6c4a98f258
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-09-24 12:01:23 -0600

    added template

---
+++ template/sketch.js
@@ -0,0 +1,17 @@
+// First Demo
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255,0,255);
+  rect(mouseX,mouseY,100,50);
+
+}

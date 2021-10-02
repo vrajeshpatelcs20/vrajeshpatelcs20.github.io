6014c5321e399a226dbae5d9d3549e84ec872cb3
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-10-02 00:30:30 -0600

    made many changes

--- SnakeGame/sketch.js
+++ SnakeGame/sketch.js
@@ -7,19 +7,22 @@
 
 
 
-// the snake is divided into small segments, which are drawn and edited on each 'draw' call
-let numSegments = 10;
+// global variables
+let numSegments = 5;
 let direction = 'right';
 
-const xStart = 0; //starting x coordinate for snake
-const yStart = 250; //starting y coordinate for snake
-const diff = 10;
 
-let xCor = [];
-let yCor = [];
 
-let xFruit = 0;
-let yFruit = 0;
+// stater cordinates and spacer
+let xStart = 0;
+let yStart = 250;
+let spaceBetweenCircles = 10;
+
+let snakeXCordinate = [];
+let snakeYCordinate = [];
+
+let appleXCordinate = 0;
+let appleYCordinate = 0;
 let scoreElem;
 
 function setup() {
@@ -30,144 +33,121 @@ function setup() {
 
   createCanvas(windowWidth, windowHeight);
   frameRate(15);
-  stroke(255);
+  stroke(255); // change color from here
   strokeWeight(10);
-  updateFruitCoordinates();
+  updateappleCoordinates();
 
   for (let i = 0; i < numSegments; i++) {
-    xCor.push(xStart + i * diff);
-    yCor.push(yStart);
+    snakeXCordinate.push(xStart + i * spaceBetweenCircles);
+    snakeYCordinate.push(yStart);
   }
 }
 
-function draw() {
-  background(0);
-  for (let i = 0; i < numSegments - 1; i++) {
-    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-  }
-  updateSnakeCoordinates();
-  checkGameStatus();
-  checkForFruit();
-}
 
-/*
- The segments are updated based on the direction of the snake.
- All segments from 0 to n-1 are just copied over to 1 till n, i.e. segment 0
- gets the value of segment 1, segment 1 gets the value of segment 2, and so on,
- and this results in the movement of the snake.
-
- The last segment is added based on the direction in which the snake is going,
- if it's going left or right, the last segment's x coordinate is increased by a
- predefined value 'diff' than its second to last segment. And if it's going up
- or down, the segment's y coordinate is affected.
-*/
+// update/change the direction of the snake
 function updateSnakeCoordinates() {
   for (let i = 0; i < numSegments - 1; i++) {
-    xCor[i] = xCor[i + 1];
-    yCor[i] = yCor[i + 1];
+    snakeXCordinate[i] = snakeXCordinate[i + 1];
+    snakeYCordinate[i] = snakeYCordinate[i + 1];
   }
-  switch (direction) {
-    case 'right':
-      xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
-      yCor[numSegments - 1] = yCor[numSegments - 2];
-      break;
-    case 'up':
-      xCor[numSegments - 1] = xCor[numSegments - 2];
-      yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
-      break;
-    case 'left':
-      xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
-      yCor[numSegments - 1] = yCor[numSegments - 2];
-      break;
-    case 'down':
-      xCor[numSegments - 1] = xCor[numSegments - 2];
-      yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
-      break;
+  if (direction === "right") {
+    snakeXCordinate[numSegments - 1] = snakeXCordinate[numSegments - 2] + spaceBetweenCircles;
+    snakeYCordinate[numSegments - 1] = snakeYCordinate[numSegments - 2];
+  }
+
+  if (direction === "up") {
+    snakeXCordinate[numSegments - 1] = snakeXCordinate[numSegments - 2];
+    snakeYCordinate[numSegments - 1] = snakeYCordinate[numSegments - 2] - spaceBetweenCircles;
+  }
+  if (direction === "left") {
+    snakeXCordinate[numSegments - 1] = snakeXCordinate[numSegments - 2] - spaceBetweenCircles;
+    snakeYCordinate[numSegments - 1] = snakeYCordinate[numSegments - 2];
+
+  }
+  if (direction === "down") {
+
+    snakeXCordinate[numSegments - 1] = snakeXCordinate[numSegments - 2];
+    snakeYCordinate[numSegments - 1] = snakeYCordinate[numSegments - 2] + spaceBetweenCircles;
+
   }
 }
 
-/*
- I always check the snake's head position xCor[xCor.length - 1] and
- yCor[yCor.length - 1] to see if it touches the game's boundaries
- or if the snake hits itself.
-*/
+// check to see if snake is out of bounds or not
 function checkGameStatus() {
   if (
-    xCor[xCor.length - 1] > width ||
-    xCor[xCor.length - 1] < 0 ||
-    yCor[yCor.length - 1] > height ||
-    yCor[yCor.length - 1] < 0 ||
+    snakeXCordinate[snakeXCordinate.length - 1] > width ||
+    snakeXCordinate[snakeXCordinate.length - 1] < 0 ||
+    snakeYCordinate[snakeYCordinate.length - 1] > height ||
+    snakeYCordinate[snakeYCordinate.length - 1] < 0 ||
     checkSnakeCollision()
   ) {
     noLoop();
-    const scoreVal = parseInt(scoreElem.html().substring(8));
+    let scoreVal = parseInt(scoreElem.html().substring(8));
     scoreElem.html('Game ended! Your score was : ' + scoreVal);
   }
 }
 
-/*
- If the snake hits itself, that means the snake head's (x,y) coordinate
- has to be the same as one of its own segment's (x,y) coordinate.
-*/
+// checker to see if the snake hits the snake
 function checkSnakeCollision() {
-  const snakeHeadX = xCor[xCor.length - 1];
-  const snakeHeadY = yCor[yCor.length - 1];
-  for (let i = 0; i < xCor.length - 1; i++) {
-    if (xCor[i] === snakeHeadX && yCor[i] === snakeHeadY) {
+  let snakeHeadX = snakeXCordinate[snakeXCordinate.length - 1];
+  let snakeHeadY = snakeYCordinate[snakeYCordinate.length - 1];
+  for (let i = 0; i < snakeXCordinate.length - 1; i++) {
+    if (snakeXCordinate[i] === snakeHeadX && snakeYCordinate[i] === snakeHeadY) {
       return true;
     }
   }
 }
 
-/*
- Whenever the snake consumes a fruit, I increment the number of segments,
- and just insert the tail segment again at the start of the array (basically
- I add the last segment again at the tail, thereby extending the tail)
-*/
-function checkForFruit() {
-  point(xFruit, yFruit);
-  if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
-    const prevScore = parseInt(scoreElem.html().substring(8));
+// checker to see if the apple is consumed or not
+function checkForapple() {
+  point(appleXCordinate, appleYCordinate);
+  if (snakeXCordinate[snakeXCordinate.length - 1] === appleXCordinate && snakeYCordinate[snakeYCordinate.length - 1] === appleYCordinate) {
+    let prevScore = parseInt(scoreElem.html().substring(8));
     scoreElem.html('Score = ' + (prevScore + 1));
-    xCor.unshift(xCor[0]);
-    yCor.unshift(yCor[0]);
+    snakeXCordinate.unshift(snakeXCordinate[0]);
+    snakeYCordinate.unshift(snakeYCordinate[0]);
     numSegments++;
-    updateFruitCoordinates();
+    updateappleCoordinates();
   }
 }
-
-function updateFruitCoordinates() {
-  /*
-    The complex math logic is because I wanted the point to lie
-    in between 100 and width-100, and be rounded off to the nearest
-    number divisible by 10, since I move the snake in multiples of 10.
-  */
-
-  xFruit = floor(random(10, (width - 100) / 10)) * 10;
-  yFruit = floor(random(10, (height - 100) / 10)) * 10;
+// place a new apple
+function updateappleCoordinates() {
+  appleXCordinate = floor(random(10, (width - 100) / 10)) * 10;
+  appleYCordinate = floor(random(10, (height - 100) / 10)) * 10;
 }
-
+// keyboard functions
 function keyPressed() {
   switch (keyCode) {
-    case 74:
+    case 65:
       if (direction !== 'right') {
         direction = 'left';
       }
       break;
-    case 76:
+    case 68:
       if (direction !== 'left') {
         direction = 'right';
       }
       break;
-    case 73:
+    case 87:
       if (direction !== 'down') {
         direction = 'up';
       }
       break;
-    case 75:
+    case 83:
       if (direction !== 'up') {
         direction = 'down';
       }
       break;
   }
+}
+
+// main
+function draw() {
+  background(0);
+  for (let i = 0; i < numSegments - 1; i++) {
+    line(snakeXCordinate[i], snakeYCordinate[i], snakeXCordinate[i + 1], snakeYCordinate[i + 1]);
+  }
+  updateSnakeCoordinates();
+  checkGameStatus();
+  checkForapple();
 }

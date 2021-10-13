fc61afacae7820182668262f5e03097138f6e1cc
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-10-13 12:07:16 -0600

    added sound

--- grid/sketch.js
+++ grid/sketch.js
@@ -1,7 +1,13 @@
 // Grid Demo
 
-let gridSize = 20;
+let gridSize = 5;
 let grid;
+let clickSound;
+
+function preload(){
+  clickSound = loadSound("assets/bruh.mp3");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = createRandom2DArray(gridSize, gridSize);
@@ -10,23 +16,24 @@ function draw() {
   background(255);
   displayGrid();
 }
+function mousePressed() {
+  clickSound.play();
 
-function mousePressed(){
   let cellWidth = width / gridSize;
   let cellHeight = height / gridSize;
 
-  let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
+  let cellX = Math.floor(mouseX / cellWidth);
+  let cellY = Math.floor(mouseY / cellHeight);
 
-  if(grid[cellY][cellX] === 1){
+  if (grid[cellY][cellX] === 1) {
     grid[cellY][cellX] = 0;
+    stroke(random(255), random(255), random(255));
   }
-  else if(grid[cellY][cellX] === 0){
+  else if (grid[cellY][cellX] === 0) {
     grid[cellY][cellX] = 1;
+    stroke(random(255), random(255), random(255));
   }
 }
-
-
 function displayGrid() {
   let cellWidth = width / gridSize;
   let cellHeight = height / gridSize;
@@ -52,14 +59,12 @@ function createEmpty2DArray(rows, cols) {
     }
   }
   return grid;
-
-
 } function createRandom2DArray(rows, cols) {
   let grid = [];
   for (let y = 0; y < rows; y++) {
     grid.push([]);
     for (let x = 0; x < cols; x++) {
-      if (random(100) < 49) {
+      if (random(100) < 0) {
         grid[y].push(0);
       }
       else {

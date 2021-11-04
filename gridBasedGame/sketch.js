dfb5283de76ca883038a017a5648792f960448ad
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-11-03 23:00:42 -0600

    made level 2 and establish a basic game
    all you need to do now is add multiple levels and a starter screen with state variables

--- gridBasedGame/sketch.js
+++ gridBasedGame/sketch.js
@@ -2,16 +2,21 @@
 
 let gridSize = 20;
 let grid;
-let water, sand, grass, wall;
+let water, sand, grass, wall, bot;
 let playerX = 0;
 let playerY = 0;
-let previousBlock;
+let previousBlock = 1;
+let blockNumber = 0;
+let stateOfGame = starterScreen
+
+
 
 function preload() {
   grass = loadImage("assets/grass.jpg");
   water = loadImage("assets/water.jpg");
   sand = loadImage("assets/sand.jpg");
   wall = loadImage("assets/wall.png");
+  bot = loadImage("assets/bot.png");
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -25,7 +30,7 @@ function draw() {
   displayGrid();
 }
 
-function keyPressed(){
+function keyPressed() {
   if (key === "s") {
     tryToMoveTo(playerX, playerY + 1);
   }
@@ -37,6 +42,18 @@ function keyPressed(){
   }
   else if (key === "d") {
     tryToMoveTo(playerX + 1, playerY);
+  }
+  if (key === "e"){
+    blockNumber = 0;
+  }
+  if (key === "r"){
+    blockNumber = 2;
+  }
+  if (key === "t"){
+    blockNumber = 3;
+  }
+  if (key === "y"){
+    blockNumber = 1;
   }
 }
 
@@ -51,11 +68,11 @@ function mousePressed() {
 
 
 function tryToMoveTo(newX, newY) {
-  previousBlock = grid[newX][newY];
-  grid[playerY][playerX] = previousBlock;
   if (newX >= 0 && newY >= 0 && newX < gridSize && newY < gridSize) {
-    if (grid[newY][newX] === 0 || grid[newY][newX] === 1 || grid[newY][newX] === 2 || grid[newY][newX] === 3) {
+    if (grid[newY][newX] === 0 || grid[newY][newX] === 1 || grid[newY][newX] === 2) {
       // reset current player spot to 0/empty  
+      grid[playerY][playerX] = previousBlock;
+      previousBlock = grid[newY][newX];
       playerX = newX;
       playerY = newY;
       grid[playerY][playerX] = 9;
@@ -65,22 +82,22 @@ function tryToMoveTo(newX, newY) {
 
 function swap(x, y) {
   if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
-    if (grid[y][x] === 0) {
-      grid[y][x] = 1;
+    if (grid[y][x] === 0 || grid[y][x] === 1 || grid[y][x] === 2 || grid[y][x] === 3) {
+      grid[y][x] = blockNumber;
       // stroke(random(255), random(255), random(255));
     }
-    else if (grid[y][x] === 1) {
-      grid[y][x] = 2;
-      // stroke(random(255), random(255), random(255));
-    }
-    else if (grid[y][x] === 2) {
-      grid[y][x] = 3;
-      // stroke(random(255), random(255), random(255));
-    }
-    else if (grid[y][x] === 3) {
-      grid[y][x] = 0;
-      // stroke(random(255), random(255), random(255));
-    }
+    // else if (grid[y][x] === 1) {
+    //   grid[y][x] = 2;
+    //   // stroke(random(255), random(255), random(255));
+    // }
+    // else if (grid[y][x] === 2) {
+    //   grid[y][x] = 3;
+    //   // stroke(random(255), random(255), random(255));
+    // }
+    // else if (grid[y][x] === 3) {
+    //   grid[y][x] = 0;
+    //   // stroke(random(255), random(255), random(255));
+    // }
   }
 }
 
@@ -89,6 +106,9 @@ function displayGrid() {
   let cellHeight = height / gridSize;
   for (let y = 0; y < gridSize; y++) {
     for (let x = 0; x < gridSize; x++) {
+      rect(width - cellWidth, height - cellHeight, cellWidth, cellHeight);
+      fill("white");
+      noStroke();
       if (grid[y][x] === 0) {
         image(water, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
       }
@@ -101,21 +121,22 @@ function displayGrid() {
       else if (grid[y][x] === 3) {
         image(wall, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
       }
+      if (grid[y][x] === 9) {
+        image(bot, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+      }
       // noStroke();
       // rect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
     }
   }
-  // rect(0,0,cellWidth,cellHeight);
-  rect(width - cellWidth, height - cellHeight, cellWidth, cellHeight);
-  fill("green");
-  noStroke();
+  // rect(0, 0, cellWidth, cellHeight);
+  
 }
 function create2DArray(rows, cols) {
   let grid = [];
   for (let y = 0; y < rows; y++) {
     grid.push([]);
     for (let x = 0; x < cols; x++) {
-      grid[y].push(0);
+      grid[y].push(1);
     }
   }
   return grid;

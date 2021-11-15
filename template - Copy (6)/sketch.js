1d43ed90bf34fdf8a56f812da313fffe12ee454f
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-11-15 11:29:50 -0600

    g

--- template - Copy (6)/sketch.js
+++ template - Copy (6)/sketch.js
@@ -1,15 +1,49 @@
-// First Demo
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Do not edit the code between these comments. 
+// You should only edit the class Particle below.
+// -----------------------------------------------
+let fireworks = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255,0,255);
+  background("black");
+
+  for (let i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].display();
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < 100; i++) {
+    let someParticle = new Particle(mouseX, mouseY);
+    fireworks.push(someParticle);
+  }
+}
+
+// -----------------------------------------------
+
+class Particle {
+  // Your code should go here!
+  // Make the class work as described in the quiz question...
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.theColor = "red";
+    this.radius = 4;
+    this.dx = random(-10, 10);
+    this.dy = random(-10, 10);
+  }
+  display() {
+    circle(this.x, this.y, this.radius * 2);
+    fill(this.theColor);
+  }
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+
+
+  }
 }

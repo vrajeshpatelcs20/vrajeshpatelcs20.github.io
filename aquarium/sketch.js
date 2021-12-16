ee79e33086279644b55c84459bbb719784775bf0
vrajeshpatelcs20 <72627530+vrajeshpatelcs20@users.noreply.github.com>
2021-12-16 11:49:03 -0600

    g

--- aquarium/sketch.js
+++ aquarium/sketch.js
@@ -1,21 +1,30 @@
 // Aquarium
 
-let fish;
-let pufferfishImg;
+let fish, octopus;
+let pufferfishImg, octopusImg;
+let numberOfRects;
+let rectHeights = [];
 
 function preload() {
   pufferfishImg = loadImage("assets/fish.png");
+  octopusImg = loadImage("assets/octopus.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fish = new Pufferfish(random(width), random(height), 500, pufferfishImg);
+  numberOfRects = width;
+  fish = new Pufferfish(random(width), random(height), 50, pufferfishImg);
+  octopus = new Octopus(100,200,100,octopusImg);
+
 }
 
 function draw() {
-  background(0);
+  background(255);
   fish.update();
+  fish.generateTerrain();
   fish.display();
+  octopus.update();
+  octopus.display();
 }
 
 class Creature {
@@ -26,7 +35,6 @@ class Creature {
   }
   update() {
     this.x += 4;
-
   }
   display() {
     fill("green");
@@ -39,11 +47,36 @@ class Pufferfish extends Creature {
   constructor(x, y, size, theImage) {
     super(x, y, size);
     this.myImage = theImage;
+    this.yTime = 1000;
   }
-  update(){
-    
+  update() {
+    this.x += 2;
+    this.y = noise(this.yTime) * height;
+    this.yTime += random(0.001, 0.005);
+
+    if (this.x > width) {
+      this.x = 0;
+    }
+  }
+  generateTerrain() {
+    let time = 0;
+    for (let i = 0; i < numberOfRects; i++) {
+      let theHeight = noise(time) * height;
+      rectHeights.push(theHeight);
+      time += 0.001;
+    }
   }
   display() {
-    image(this.myImage, this.x, this.y, this.size,this.size);
+    image(this.myImage, this.x, this.y, this.size, this.size);
+  }
+}
+
+class Octopus extends Creature {
+  constructor(x, y, size, someImage) {
+    super(x, y, size);
+    this.image = someImage;
+  }
+  display() {
+    image(this.image, this.x, this.y, this.size, this.size);
   }
 }
